f63f4b62f9d8cf67f01c2dceacf00ddde6a5dcf3
Evgeny-379 <evgeny-379@yandex.ru>
2025-01-09 23:52:13 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+print("1st program")
+print(9 ** 0.5 * 5)
+
+print("2st program")
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+print("3st program")
+a = 2 * 2 + 2
+b = 2 * (2 + 2)
+print(a == b)
+
+print("4st program")
+a = '123.456'
+a = float(a)
+a = a * 10
+a = int(a)
+a = a % 10
+print(a)
